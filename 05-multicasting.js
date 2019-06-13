a56cfac1ece33fa8ec27da953d0bbfbc118c236b
Mihai Rotaru <mihai.rotaru@gmx.com>
2019-06-13 07:21:54 +0100

    cannot have mutliple active subscribers

---
+++ 05-multicasting.js
@@ -0,0 +1,33 @@
+const { Observable, of } = require("rxjs");
+const { map, filter, tap, take } = require("rxjs/operators");
+
+let source$
+
+// Normally, you can only have one active subscriber. If you add a second one,
+// the observable will be re-created; the two subscribers (observers) will not
+// receive the same values. Below, _each_ observable will get it's own 1, 2, 3:
+source$ = of(1, 2, 3)
+source$.subscribe(v => console.log('subscriber 1', v))
+source$.subscribe(v => console.log('subscriber 2', v))
+
+// Same thing is better illustrated below; observers will not get the same
+// numbers. Well, they might, but that is very unlikely: the observable will
+// re-run on every subscription, generating another set of values, which may or
+// may not be the same.
+
+// This is what's known as a "cold" observable. It gets cold after running;
+// but will "warm up" on each subscription. Multiple subscribers will not have
+// access to the same "running", or "warmed up", observable.
+
+const getRandomInt = max => Math.floor(Math.random() * Math.floor(max))
+
+source$ = new Observable(subscriber => {
+    console.log('Observable running !')
+    let count = 0 
+    while(count < 3) {
+        subscriber.next(getRandomInt(1000))
+        count++
+    }
+})
+source$.subscribe(v => console.log('subscriber 1', v))
+source$.subscribe(v => console.log('subscriber 2', v))
